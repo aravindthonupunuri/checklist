114c251ef16cb09d1c2cb35a685dadd57cf1a0c7
Mohana Sundaram <MohanaSundaram.A@target.com>
2021-05-12 13:56:48 +0530

    Unified source changes across apps (#78)

--- backpack-registrychecklists-migration-consumer-app/src/test/functional/groovy/com/tgt/backpackregistrychecklists/kafka/api/CheckListMarkNotifyEventHandlerFunctionalTest.groovy
+++ backpack-registrychecklists-migration-consumer-app/src/test/functional/groovy/com/tgt/backpackregistrychecklists/kafka/api/CheckListMarkNotifyEventHandlerFunctionalTest.groovy
@@ -213,7 +213,7 @@ class CheckListMarkNotifyEventHandlerFunctionalTest extends BaseKafkaFunctionalT
                             checklistTemplateRepository.save(checklistTemplate).block()
                             return true
                         }
-                        if (eventHeaders.source == "backpackregistrychecklists-migration") {
+                        if (eventHeaders.source == "backpack-registrychecklists") {
                             return true
                         }
                     }
