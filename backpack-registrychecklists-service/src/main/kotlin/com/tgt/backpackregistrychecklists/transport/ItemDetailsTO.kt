ea2cad70d9676dd2f46912d12c984e398b39f940
aravindthonupunuri <Aravind.Thonupunuri@target.com>
2021-05-21 16:04:44 +0530

    changing date format (#82)
    Co-authored-by: Aravind <aravind.thoupunuri@target.com>

--- backpack-registrychecklists-service/src/main/kotlin/com/tgt/backpackregistrychecklists/transport/ItemDetailsTO.kt
+++ backpack-registrychecklists-service/src/main/kotlin/com/tgt/backpackregistrychecklists/transport/ItemDetailsTO.kt
@@ -1,5 +1,10 @@
 package com.tgt.backpackregistrychecklists.transport
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import java.time.LocalDateTime
 
 data class ItemDetailsTO(
@@ -7,6 +12,12 @@ data class ItemDetailsTO(
     val description: String? = null,
     val imageUrl: String? = null,
     val alternateImageUrls: List<String>? = null,
+    @JsonDeserialize(using = LocalDateTimeDeserializer::class)
+    @JsonSerialize(using = LocalDateTimeSerializer::class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val addedTs: LocalDateTime? = null,
+    @JsonDeserialize(using = LocalDateTimeDeserializer::class)
+    @JsonSerialize(using = LocalDateTimeSerializer::class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val lastModifiedTs: LocalDateTime? = null
 )
